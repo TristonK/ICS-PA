1e0d1e7244d05a09cf95b156542519ef5a4a1515
tracer-ics2018 <tracer@njuics.org>
2018-09-15 07:29:56 +0000

    >  compile 171240501 171240501 Linux 508c27a6374b 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  07:29:56 up  5:04,  1 user,  load average: 0.00, 0.00, 0.00 fb41c764cc809433f27544e7b0e53e2ebb916330

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -48,9 +48,9 @@ static int cmd_si(char *args) {
 static int cmd_info(char *args){
 	if (args[0]=='r'){
 		for(int i=R_EAX;i<=R_EDI;i++){
-			printf("%s: 0x %8x\n",regsl[i],reg_l(i));
+			printf("%s: 0x %08x\n",regsl[i],reg_l(i));
 	 	 	} 
-		printf("eip:0x%8x\n",cpu.eip);
+		printf("eip:0x%08x\n",cpu.eip);
 	 	} 
 	return 0; 
 }
@@ -63,13 +63,14 @@ static int cmd_x(char *args){
 	sscanf(N,"%d",&n);
 	char *exprr =strtok(NULL," ");
     sscanf(exprr,"%x",&addre);
-    for(int i=0;i<n;i++){ 
+    for(int i=0;i<n;i++) { 
 		printf("0x%x ",vaddr_read(addre,4));
 		addre+=4;
 		}
 	printf("\n");
 	return 0;  
-	}
+}
+
 static int cmd_help(char *args);
 
 static struct {
