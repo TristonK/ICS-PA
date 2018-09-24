e692efe863fee33841de7bb424b9c162271eb635
tracer-ics2018 <tracer@njuics.org>
2018-09-24 01:09:47 +0000

    >  compile 171240501 171240501 Linux 508c27a6374b 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  01:09:47 up 24 min,  4 users,  load average: 0.51, 0.53, 0.28 193bae8d27bdf7d43825c8af5b9454746a46e2af

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -141,13 +141,14 @@ int find_main_op(int p,int q){
 			op++;
 			} 
  		}
+	printf("%c",tokens[opr].type);
 	return opr;
 	}
 uint32_t eval(int p,int q){
  	if(p>q){ 
         printf("tokens path error");
 		return -1;} 
- 	else if ( p==q){
+ 	else if (p==q){
 		if(tokens[p].type==DEC){
  			return atoi(tokens[p].str);}
  		else{ 
