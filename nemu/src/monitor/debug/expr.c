676a1f1b6b4a8d4efe59bb518de39186f2877bcd
tracer-ics2018 <tracer@njuics.org>
2018-09-29 02:55:14 +0000

    >  compile 171240501 171240501 Linux 508c27a6374b 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  02:55:14 up  2:29,  5 users,  load average: 0.12, 0.24, 0.20 88c816c8768eb66358bf99da8983407ac94154dd

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -71,7 +71,8 @@ static bool make_token(char *e) {
 
   while (e[position] != '\0') {
     /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i ++) {
+    printf("while");
+	for (i = 0; i < NR_REGEX; i ++) {
        if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
@@ -84,7 +85,7 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-
+          printf("for");
          switch (rules[i].token_type) {
 			case TK_NOTYPE: break;
           default:
