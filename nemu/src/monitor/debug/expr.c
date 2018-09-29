d5db639ee8e999994c31b1232a8548cba4ba279f
tracer-ics2018 <tracer@njuics.org>
2018-09-29 02:51:55 +0000

    >  compile 171240501 171240501 Linux 508c27a6374b 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  02:51:55 up  2:26,  5 users,  load average: 0.11, 0.21, 0.18 3206737d6d4f02b51c6298f0a9c3ce979c8bd625

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -171,7 +171,9 @@ uint32_t eval(int p,int q){
 			case '-' : return val1-val2;
 			case '*' : return val1*val2;
 			case '/' : return val1/val2;
-			default: assert(0);
+			default:
+			       printf("wrong operation\n"); 
+			       assert(0);
   		 	}
   		} 
 		return 0;
