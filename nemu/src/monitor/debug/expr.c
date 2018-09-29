1f05a77322e06cc2cc397b9cc587452409540d8a
tracer-ics2018 <tracer@njuics.org>
2018-09-29 17:27:18 +0000

    >  compile 171240501 171240501 Linux 508c27a6374b 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  17:27:18 up 13:15,  8 users,  load average: 0.04, 0.05, 0.02 de6c71b4f67b703b1d5f96df938e3882c1db948

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -248,7 +248,7 @@ uint32_t expr(char *e, bool *success) {
   //printf("pointer is ok");
   /* TODO: Insert codes to evaluate the expression. */
   for(int i=0;i<nr_token;i++){
-	  if(tokens[i].type=='*'&&(i==0||(tokens[i-1].type!=DEC&&tokens[i-1].type!=HEX)))
+	  if(tokens[i].type=='*'&&(i==0||(tokens[i-1].type!=DEC&&tokens[i-1].type!=HEX&&tokens[i-1].type!=')')))
 	    tokens[i].type=DEREF;
 	  }
   return eval(0,nr_token-1);
