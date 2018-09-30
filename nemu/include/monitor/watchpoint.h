b229a050ca1fd94a8f1dd01167578cc1d0f33e76
tracer-ics2018 <tracer@njuics.org>
2018-09-30 03:06:03 +0000

    >  compile 171240501 171240501 Linux 508c27a6374b 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  03:06:03 up  2:15,  8 users,  load average: 0.07, 0.04, 0.00 a24e742d4372e522c8c61550c519cbe491c3888b

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -6,10 +6,11 @@
 typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
-
+  uint32_t val;
+  char exp[32];
   /* TODO: Add more members if necessary */
-
-
+  bool used;
 } WP;
-
+WP* new_up();
+void free_up();
 #endif
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -11,6 +11,7 @@ void init_wp_pool() {
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = &wp_pool[i + 1];
+	wp_pool[i].used=false;
   }
   wp_pool[NR_WP - 1].next = NULL;
 
@@ -20,4 +21,53 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* new_up(char *args){
+	WP *temp;//,*headco;
+	//headco=head;
+	if(free_==NULL){
+		printf("no free watchpoints\n");
+		assert(0);
+		}
+	temp=free_;
+	strcpy(temp->exp,args);
+	bool succ=1;
+	uint32_t val=expr(temp->exp,&succ);
+	if(!succ){
+		printf("wrong input");
+		assert(0);
+		}
+	temp->next=head;
+	temp->val=val;
+	temp->used=true;
+	free_=free_->next;
+	head=temp;
+	/*if(head==NULL){
+		head=temp;
+		head->next=NULL;
+		}
+	else {
+		while(headco->next!=NULL)
+			headco=headco->next;
+		headco->next=temp;
+		}*/
+	return temp;
+	}
+void free_wp(WP *wp){
+	//WP *temp;
+//	temp=free_;
+	wp->used=false;
+	wp->next=free_;
+	memset(wp->exp,'\0',sizeof(wp->exp));
+	free_=wp;
+	/*if(free_==NULL){
+		free_=wp;
+		}
+    else{
+		while(temp->next!=NULL)
+			temp=temp->next;
+		temp->next
+		}*/
+	}
 
+
+	
