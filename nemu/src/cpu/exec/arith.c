d18ed21cf3bc46ba39792048171a95e016105ade
tracer-ics2018 <tracer@njuics.org>
2018-11-04 10:17:33 +0800

    >  compile 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:17:33 up 11:02,  7 users,  load average: 0.27, 0.44, 0.33 c484b35b1c840821a1b6440fc6665692f0a652d9

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -17,16 +17,27 @@ make_EHelper(sub) {
 	}
 	else*/
    //  printf("%8x %8x \n",id_dest->val,id_src->val);
-		rtl_sub(&t1,&id_dest->val,&id_src->val);
-    operand_write(id_dest,&t1);
+	rtl_sub(&t2,&id_dest->val,&id_src->val);
+    rtl_setrelop(RELOP_LTU, &t3, &id_dest->val, &t2);
+   	operand_write(id_dest,&t2);
   
   rtl_update_ZFSF(&t1,id_dest->width);
-  
+  rtl_setrelop(RELOP_LTU, &t0, &id_dest->val, &t2);
+  rtl_or(&t0, &t3, &t0);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
+
+
   print_asm_template2(sub);
 }
 
 make_EHelper(cmp) {
-  rtl_sext(&t0,&id_src->val,id_src->width);
+ /* rtl_sext(&t0,&id_src->val,id_src->width);
   rtl_sub(&t1,&id_dest->val,&t0);
   
   rtl_update_ZFSF(&t1,id_dest->width);
@@ -41,6 +52,22 @@ make_EHelper(cmp) {
   rtl_and(&t1,&t2,&t1);
   rtl_msb(&t1,&t1,id_dest->width);
   rtl_set_OF(&t1);
+*/
+	rtl_sub(&t2,&id_dest->val,&id_src->val);
+    rtl_setrelop(RELOP_LTU, &t3, &id_dest->val, &t2);
+   //	operand_write(id_dest,&t2);
+  
+  rtl_update_ZFSF(&t1,id_dest->width);
+  rtl_setrelop(RELOP_LTU, &t0, &id_dest->val, &t2);
+  rtl_or(&t0, &t3, &t0);
+  rtl_set_CF(&t0);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
+
 
   print_asm_template2(cmp);
 }
