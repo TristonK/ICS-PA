ea316df2a12c2df0b0c625db097726912ea6e0ac
tracer-ics2018 <tracer@njuics.org>
2018-11-04 10:23:44 +0800

    >  compile 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:23:44 up 11:08,  7 users,  load average: 0.37, 0.44, 0.37 8a20408994d8db8db478dc54dfc62eaafb898c9

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -58,12 +58,12 @@ make_EHelper(cmp) {
    //	operand_write(id_dest,&t2);
   
   rtl_update_ZFSF(&t1,id_dest->width);
-  rtl_setrelop(RELOP_LTU, &t0, &id_dest->val, &t2);
+  rtl_setrelop(RELOP_LTU, &t0, &t2, &t2);
   rtl_or(&t0, &t3, &t0);
   rtl_set_CF(&t0);
 
-  rtl_xor(&t0, &id_dest->val, &id_src->val);
-  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_xor(&t0, &t2, &id_src->val);
+  rtl_xor(&t1, &t2, &t2);
   rtl_and(&t0, &t0, &t1);
   rtl_msb(&t0, &t0, id_dest->width);
   rtl_set_OF(&t0);
