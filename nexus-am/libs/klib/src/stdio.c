a6bcf23157731b8a9c0e04fa467c64867f829e94
tracer-ics2018 <tracer@njuics.org>
2018-11-19 22:40:55 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:40:55 up  1:58,  6 users,  load average: 0.34, 0.13, 0.08 ada37473f1d566ec23fc5a0f60992f8390a9a2e1

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -4,15 +4,29 @@
 #ifndef __ISA_NATIVE__
 
 int printf(const char *fmt, ...) {
-  return 0;
+  char pbuf[1024];
+  va_list ap;
+  int i;
+  va_start(ap,fmt);
+  i=vsprintf(pbuf,fmt,ap);
+  va_end(ap);
+  //puts(pbuf);
+  return i;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
+  
+
   return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+    va_list ap;
+	int i;
+	va_start(ap,fmt);
+    i=vsprintf(out,fmt,ap);
+	va_end(ap);
+  	return i;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
