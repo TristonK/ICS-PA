e9ca3bce69b0d71bd85b1b683a009c2cd3acba5a
tracer-ics2018 <tracer@njuics.org>
2018-11-20 08:44:52 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  08:44:52 up  2:22,  6 users,  load average: 0.28, 0.25, 0.18 a9f191fbe60015cd2bfff92e4818b38e302e3f19

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -15,7 +15,42 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  
+  char * str;
+  const char* s;
+  int num;
+  static char numtrans[16]="0123456789ABCDEF";
+  for(str=out;*fmt;fmt++){
+	  if(*fmt!='%'){
+		  *str++=*fmt++;
+		  continue;
+	  }
+	  ++fmt;
+	  switch(*fmt){
+		  case 's':
+			  s=va_arg(ap,char *);
+			  while(*s)
+				  *str++=*s++;
+			  break;
+		 case 'd':
+			  num=va_arg(ap,int);
+			  int i=0;char tmp[20];
+			  if(num==0){
+				  *str++='0';
+				  break;
+			  }
+			  while(num){
+				  tmp[i]=numtrans[num%10];
+				  num/=10;
+				  i++;
+			  }
+			  while(i){
+				  i--;
+				  *str++=tmp[i];
+			  }
+			  break;
+		default:;
+	  }
+  }
 
   return 0;
 }
