9ad565f17db2a83f000e1120d4c7ea7ac617ab21
tracer-ics2018 <tracer@njuics.org>
2018-11-21 20:07:05 +0800

    >  compile 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:07:05 up 16:24,  5 users,  load average: 0.34, 0.25, 0.26 5fb1cf56a301f832916f76cd272ab83280267b8

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -2,12 +2,14 @@
 #include <x86.h>
 #include <amdev.h>
 
+#define I8042_DATA_PORT 0x60
+
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      kbd->keydown = inl(I8042_DATA_PORT)==_KEY_NONE?0:1;
+      kbd->keycode = inl(I8042_DATA_PORT);
       return sizeof(_KbdReg);
     }
   }
