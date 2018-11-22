538bf11a3bb55fdf59e7fbf5cd2d15aea889aad0
tracer-ics2018 <tracer@njuics.org>
2018-11-22 17:17:50 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:17:50 up 19:04,  5 users,  load average: 0.64, 0.23, 0.10 13bccd50720769a5f601610facbb073fe8dc0932

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -27,7 +27,7 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
 	  for(i=0;i<size;i++) fb[i]=i;*/
 	  int heigh=screen_height();
 	  int weigh=screen_width();
-	  for(int i=0;i<heigh;i++){
+	  for(int i=0;i<=heigh;i++){
 		  for(int j=0;j<=weigh;j++){
               fb[i*weigh+j]=ctl->pixels[i*weigh+j];
 		  }
