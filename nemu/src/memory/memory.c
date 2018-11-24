856599e8fd3d6e4bca420ccd9c3abb06a58b1a13
tracer-ics2018 <tracer@njuics.org>
2018-11-24 20:04:51 +0800

    >  compile 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:04:51 up 23:56,  4 users,  load average: 0.59, 0.67, 0.60 febbcb91c1a9d4c6b736f5ebb4ae70eb3a05dce2

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,30 +1,30 @@
 #include "nemu.h"
-#include "device/mmio.h"
+
 #define PMEM_SIZE (128 * 1024 * 1024)
+uint32_t mmio_read(paddr_t addr,int len,int map_NO);
+void mmio_write(paddr_t addr,int len,uint32_t data,int map_NO);
+int is_mmio(paddr_t addr);
 
 #define pmem_rw(addr, type) *(type *)({\
-    Assert(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound", addr); \
+    Assert(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound,cpu.eip:%x", addr,cpu.eip); \
     guest_to_host(addr); \
     })
+#define MMIO_SPACE_MAX (512*1024)
 
 uint8_t pmem[PMEM_SIZE];
 
-/* Memory accessing interfaces */
+/* Memory accessing interfaces */ 
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  int port=is_mmio(addr);
-  if(port==-1)
-	  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
-  else
-	  return mmio_read(addr,len,port);
+	int port=is_mmio(addr);
+	if(port!=-1)return mmio_read(addr,len,port); 	
+	return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, uint32_t data, int len) {
-  int port=is_mmio(addr);
-  if(port==-1)
-	  memcpy(guest_to_host(addr), &data, len);
-  else
-	  mmio_write(addr,len,data,port);
+  	int port=is_mmio(addr);
+	if(port!=-1)mmio_write(addr,len,data,port);
+	else memcpy(guest_to_host(addr), &data, len);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
