ab8f922ca29c3e018db065434d586379acb0d159
tracer-ics2018 <tracer@njuics.org>
2018-11-24 19:34:46 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:34:46 up 23:26,  4 users,  load average: 0.56, 0.50, 0.52 11c7dd35e16fcfd7df08fea4e814bc014c82d390

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -1,98 +1,110 @@
 #include "klib.h"
 
 //#ifndef __ISA_NATIVE__
-#if !defined(__ISA_NATIVE__)||defined(__NATIVE_USE_KLIB__)
-size_t strlen(const char *s) {
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-	const char *t=s;
-	while(*t++);
-	return (size_t)(t -s -1);
+size_t strlen(const char *s) {
+   /* const char *sc=s;
+	while(*sc!='\0'){
+		sc++;
+	}
+  	return (sc-s);*/
+	int count=0;
+	while(*s){
+		count++;
+		s++;
+	}
+	return count;
 }
 
 char *strcpy(char* dst,const char* src) {
-	char * start=dst;
-	while(*src)
-	{
-		*dst=*src;
-		dst++;
-		src++;
-	}
-	*dst='\0';
-  return start;
+   /* char *s=dst;
+	for(s=dst;(*s++=*src++)!='\0';);
+	return (dst);*/
+	char *s=dst;
+	while((*dst++=*src++)!='\0');
+	return s;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-	size_t i;
-	for(i=0;i<n &&src[i]!='\0';i++)
-		dst[i]=src[i];
-	for(;i<n;i++)
-		dst[i]='\0';
-	return dst;
+	char *s=dst;
+	while(n--){
+		if(*src!='\0')
+			*dst++=*src++;
+		else
+			*dst++='\0';
+	}
+  return s;
 }
 
 char* strcat(char* dst, const char* src) {
-	char *t=dst;
-       while(*t)t++;//find the end of dst;
-	while( (*t=*src) ){
-		src++;
-		t++;
-	}
-	return	dst;//src应该有终止空字符，此处不再多加，可能。。
+	/*char *s;
+	for(s=dst;*s!='\0';s++);
+	for(;(*s=*src)!='\0';++s,src++);
+    return dst;*/
+	char *s=dst;
+	while(*dst!='\0')
+		++dst;
+	while((*dst++=*src++)!='\0');
+	return s;
 }
 
 int strcmp(const char* s1, const char* s2) {
-
-	while(*s1==*s2 )
-	{
-		if(*s1=='\0')return 0;
-		else ++s1,++s2;
+	/*for(;*s1==*s2;s1++,s2++)
+		if(*s1=='\0')
+			return 0;
+	return ((*(unsigned char *)s1<*(unsigned char *)s2)?-1:+1);*/
+	while(*s1&&*s2&&*s1==*s2){
+		++s1;
+		++s2;
 	}
-	if(*(unsigned char *)s1<*(unsigned char *)s2)return -1;
-	else if(*(unsigned char *)s1>*(unsigned char *)s2)return 1;
-	else return 0;
+	return (*s1-*s2);
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-	//return >0 if s1>s2
-  	if(!n)return 0;
-	while( n)
-	{
-		if(*s1==*s2)s1++,s2++;
-		else break;
-		n--;
-	}
-	if(!n)return 0;
-	if(*(unsigned char *)s1>*(unsigned char *)s2)return 1;
-	else return -1;
+   //may wrong
+	for(;n>0;++s1,++s2,--n)
+		if(*s1!=*s2)
+			return ((*(unsigned char *)s1<*(unsigned char *)s2)?-1:+1);
+		else if(*s1=='\0')
+		     return -1;
+	return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-	void *start=v;
-	while(n--){
-		*(char *)v=(char)c;
-		(char *)v++;
-	}
-	return start;
+ /* const unsigned char uc=c;
+  unsigned char *su;
+  for(su=v;n>0;su++,n--)
+      *su=uc;
+   return v;*/
+   char * su=v;
+   while(n--){
+	   *su++=c;
+   }
+   return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-	void *ret=out;
-	while(n--){
-		*(char *)out=*(char *)in;
-		(char *)out++;
-		(char *)in++;
-	}
-	return ret;
+  //changed
+  void *rett=out;
+  char *su1=(char *)out;
+  const char *su2=(char *)in;
+  for(;n>0;n--)
+       *su1++=*su2++;
+  return rett;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-	if(!n)return 0;
-	while(--n && *(char *)s1 == *(char *)s2)
-	{
-		(char *)s1++;
-		(char *)s2++;	
+ /* const unsigned char *su1,*su2;
+  for(su1=s1,su2=s2;n>0;su1++,su2++,n--)
+	  return ((*su1<*su2)?-1:+1);
+  return (0);*/
+	const unsigned char *su1=s1,*su2=s2;
+	while(n--){
+		if(*su1++!=*su2++)
+			return (*--su1-*--su2);
 	}
-	return (*(unsigned char *)s1- *(unsigned char *)s2);	
+	return 0;
 }
 
 #endif
