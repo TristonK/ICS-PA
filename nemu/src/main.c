cf9978d18bf4560851be4c7f4474abe961feb61e
tracer-ics2018 <tracer@njuics.org>
2018-11-24 20:15:39 +0800

    >  compile 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:15:39 up 1 day, 7 min,  6 users,  load average: 0.90, 0.55, 0.57 85a3a201e8a2f11659d46aee9013858a7c05b05

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,31 +1,39 @@
 #include<stdio.h>
 #include<nemu.h>
-#include<./monitor/expr.h>
+#include<monitor/expr.h>
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
-
+uint32_t expr(char *e,bool *success);
+void test_expr();
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  int is_batch_mode = init_monitor(argc, argv);
-/*  FILE *fp=fopen("/home/ksl/ics2018/nemu/tools/gen-expr/input","r");
-  //assert(fp!=NULL);
-  uint32_t mach,me;
-  char ex[500];
-  int cnttt=0;
-  for(int i=1;i<=100;i++){
-      memset(ex,'\0',sizeof(ex));
-	  fscanf(fp,"%u",&mach);
-	  fscanf(fp,"%[^\n]",ex);
-	  bool succ=1;
-	  me=expr(ex,&succ);
-	 // printf("%dth expr: ",i);
-	   if(me!=mach){
-	  printf("answer is WRONG!,right is %u and yours is %u\n",mach,me);cnttt++;}  
-	  }
-printf("you have %d expression get wrong answer.\n",cnttt);
- fclose(fp);*/ 
-  /* Receive commands from user. */
+ 	 int is_batch_mode = init_monitor(argc, argv);
+	//test_expr();
+   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
   return 0;
 }
+void test_expr(){
+	 FILE *fp=fopen("/home/tengwang/ics2018/nemu/tools/gen-expr/input","r");
+  		uint32_t ans1;
+		uint32_t ans2;
+		char expre[400];
+		int cnt=1;
+	while(fscanf(fp,"%u",&ans1)!=EOF)
+	{
+		bool success=1;	
+		printf("%d:",cnt++);
+		fscanf(fp,"%[^\n]",expre);
+		ans2=expr(expre,&success);
+		if(ans1==ans2)printf("True\n");
+		else {
+			printf("Flase.input:%u,your:%u,%s\n",ans1,ans2,expre);
+		}
+	}
+	fclose(fp);
+
+
+
+}
+
