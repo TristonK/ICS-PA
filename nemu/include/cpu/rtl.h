c8d3c9567012db86d720e7010f8a0454dfa2bb4c
tracer-ics2018 <tracer@njuics.org>
2018-11-24 21:47:26 +0800

    >  compile 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:47:26 up 1 day,  1:39,  6 users,  load average: 0.36, 0.21, 0.24 96a257352d4179baaef1fa0f7f6588ad6dce210

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -12,7 +12,7 @@ void decoding_set_jmp(bool is_jmp);
 bool interpret_relop(uint32_t relop, const rtlreg_t src1, const rtlreg_t src2);
 
 /* RTL basic instructions */
- void interpret_rtl_li(rtlreg_t* dest, uint32_t imm) {
+static inline void interpret_rtl_li(rtlreg_t* dest, uint32_t imm) {
 
   *dest = imm;
 }
