4a8dd836dd832f3af8aa67cb050dfda275749c52
tracer-ics2018 <tracer@njuics.org>
2018-11-26 23:48:43 +0800

    >  compile 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  23:48:43 up 1 day,  6:31,  7 users,  load average: 0.54, 0.29, 0.24 37df23d4286a73f16bd5a42716097a7eec42f9a

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -15,9 +15,10 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_addi(&t0,&t0,4);
   rtl_lm(&t2,&t0,4);
   t0=((t1&0x0000ffff)|(t2&0xffff0000));
-  rtl_j(t0); 
- /* decoding.jmp_eip=t0;
-  decoding.is_jmp=1;*/ 
+ // rtl_j(t0); 
+  decoding.jmp_eip=t0;
+ // decoding.is_jmp=1; 
+  rtl_j(decoding.jmp_eip);
 }
 
 void dev_raise_intr() {
