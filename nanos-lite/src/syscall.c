e13a03e384f045a3b24f9ee34fe180e47eb21510
tracer-ics2018 <tracer@njuics.org>
2018-12-08 21:44:40 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:44:40 up 2 days, 12:53, 12 users,  load average: 0.34, 0.33, 0.36 c09dba9a250a31dbfceda50c876e5c18a1651832

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,8 +7,9 @@ _Context* do_syscall(_Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-  Log("here");
+  Log("%d  %d  %d  %d\n",a[0],a[1],a[2],a[3]);
   switch (a[0]) {
+//	case SYS_exit: _halt(a[1]);break;
 	case SYS_yield: _yield();break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
