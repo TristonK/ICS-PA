efff397e3f70174f1f9554d6850e2b28a34ef2d1
tracer-ics2018 <tracer@njuics.org>
2018-12-08 11:51:49 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:51:49 up 2 days,  8:00,  8 users,  load average: 0.29, 1.02, 0.85 81cb39de87fa7bfd7bba13b2d56f008aac0df4c6

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -2,7 +2,6 @@
 #define __FS_H__
 
 #include "common.h"
-//#include <u>
 
 #ifndef SEEK_SET
 enum {SEEK_SET, SEEK_CUR, SEEK_END};
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,13 +1,8 @@
 #include "common.h"
-//#include "syscall.h"
-_Context* do_syscall(_Context *c);
 
- static _Context* do_event(_Event e, _Context* c) {
+static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-   case _EVENT_YIELD:/* printf("_event_yield found\n");*/break;
-	case _EVENT_SYSCALL: /*printf("_event_syscall found\n");*/
-							do_syscall(c);break;
-
+	case _EVENT_YIELD: printf("event yield\n");break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
