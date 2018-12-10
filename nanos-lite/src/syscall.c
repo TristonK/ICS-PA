703f64c69f0af901c84df1651121b42bf6bc45e4
tracer-ics2018 <tracer@njuics.org>
2018-12-10 10:13:28 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:13:28 up 2 days, 17:51, 12 users,  load average: 0.31, 0.41, 0.35 95c160ebbc6ac61a5f318725d7acbfea30ded6c0

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -20,20 +20,19 @@ _Context* do_syscall(_Context *c) {
 	case SYS_exit: _halt(a[1]);c->GPR1=0;break;
 	case SYS_yield: _yield();c->GPR1=0;break;
     case SYS_open: c->GPR1=fs_open((char *)a[1],0,0);break;
-    case SYS_read: fs_read(a[1],(char*)a[2],a[3]);
- 	case SYS_write:
+    case SYS_read: fs_read(a[1],(char*)a[2],a[3]);break;
+ 	case SYS_write: fs_write(a[1],(char *)a[2],a[3]);break;
     //*****SYS_write****	
 	//intptr_t ret=0;
-	if((int)a[1]==1||(int)a[1]==2){
+/*	if((int)a[1]==1||(int)a[1]==2){
 	   c->GPR1=a[1]==1?a[3]:-1;
 	   void *buf=(void *)a[2];
 	   for(int i=0;i<a[3];i++){
 	      _putc((char)*((char*)(buf+i)));
 	   }
 	}
-	Log();
+	Log();*/
 	//c->GPR1=ret;
-    break;	
 	//******************
 	case SYS_close: c->GPR1=fs_close(a[1]);break;
     case SYS_lseek: c->GPR1=fs_lseek(a[1],a[2],a[3]);
