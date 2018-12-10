cfed29508bb13769ee4decea326bb8d39295f1ff
tracer-ics2018 <tracer@njuics.org>
2018-12-10 18:27:34 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  18:27:34 up 2 days, 21:25, 12 users,  load average: 0.82, 0.50, 0.34 b6f7e3fcee75d2fbb63db6aae3f12b2ebe46abce

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -41,6 +41,7 @@ void init_device() {
   _ioe_init();
   Log("%d %d",screen_width(),screen_height());
    sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d\n\0",screen_width(),screen_height());
-  // TODO: print the string to array `dispinfo` with the format
+  Log("%s",dispinfo);
+   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
 }
