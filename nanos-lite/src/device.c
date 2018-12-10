db7f985f9d76b99aa715401964f535f9de02b7de
tracer-ics2018 <tracer@njuics.org>
2018-12-11 00:29:54 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:29:54 up  1:08,  3 users,  load average: 0.91, 0.77, 0.56 5c7ccb152f10d45070331c38491dd361c31a2329

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -20,7 +20,7 @@ static const char *keyname[256] __attribute__((used)) = {
 size_t events_read(void *buf, size_t offset, size_t len) {
     int reallen=0;
 	char temp[2001];
-	Log("in read and len is %d",len);
+//	Log("in read and len is %d",len);
   //  while(reallen<=len){
 		memset(temp,'\0',sizeof(temp));
 	    int keycode=read_key();
@@ -37,9 +37,9 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 			sprintf(temp,"t %d\n",time);
 		    }
 		int newlen=strlen(temp);
-		Log("strlen is %d",newlen);
+//		Log("strlen is %d",newlen);
 		if(newlen+reallen<len){
-			sprintf(buf+reallen,"%s",temp);
+			sprintf(buf+reallen,"%s\n",temp);
 			reallen+=newlen;
 		}
 //		else
