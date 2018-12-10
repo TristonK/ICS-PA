d2afb65641507743aa168c631f47240786106a40
tracer-ics2018 <tracer@njuics.org>
2018-12-10 21:26:05 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:26:05 up 3 days, 23 min, 11 users,  load average: 0.32, 0.49, 0.33 bf88e4fa88a85dff8e95978cae51daa412b6254

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -20,6 +20,7 @@ static const char *keyname[256] __attribute__((used)) = {
 size_t events_read(void *buf, size_t offset, size_t len) {
     int reallen=0;
 	char temp[200];
+	Log("in read");
     while(reallen<=len){
 		memset(temp,'\0',sizeof(temp));
 	    int keycode=read_key();
