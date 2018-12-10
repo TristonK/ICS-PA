5780b7d638bdb6e610a9ad84d6940a7225c8368a
tracer-ics2018 <tracer@njuics.org>
2018-12-10 11:23:07 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:23:07 up 2 days, 19:00, 12 users,  load average: 0.97, 0.47, 0.26 d67a9d98bbc72e4b5afa1d3c48174f0b5cc3c782

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,7 +2,11 @@
 #include <amdev.h>
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+    for(int i=0;i<len;i++){
+	   _putc((char)*((char*)(buf+i)));
+	}
+	return len;
+	// return 0;
 }
 
 #define NAME(key) \
