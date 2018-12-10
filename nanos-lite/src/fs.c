fa7830ff0c0893878ad96e9819fc6b9d757e7bcf
tracer-ics2018 <tracer@njuics.org>
2018-12-10 19:52:29 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:52:29 up 2 days, 22:50, 12 users,  load average: 0.30, 0.33, 0.39 574de19e27d84d332f20123ea9960c8cfe38b7d2

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -39,7 +39,7 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stderr", 0, 0,0, invalid_read, serial_write},
 #include "files.h"
   {"/dev/fb",0,0,0,invalid_read,fb_write},
-  {"/proc/dispinfo",0,0,0,dispinfo_read,invalid_write},
+  {"/proc/dispinfo",128,0,0,dispinfo_read,invalid_write},
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
