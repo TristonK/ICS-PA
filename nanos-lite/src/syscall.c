b191e918095733b4de6c0f28c0d44a3b07c5b467
tracer-ics2018 <tracer@njuics.org>
2018-12-10 10:06:10 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  10:06:10 up 2 days, 17:44, 12 users,  load average: 0.59, 0.35, 0.29 ac8d52616b9ba9e0c5822ef3fea29a8519898c63

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,13 @@
 #include "common.h"
 #include "syscall.h"
+#include <sys/types.h>
+
+int fs_open(const char* pathname, int flags, int mode);
+ssize_t fs_read(int fd, void *buf, size_t len);
+ssize_t fs_write(int fd, const void *buf, size_t len);
+off_t fs_lseek(int fd, off_t offset,int whence);
+int fs_close(int fd);
+
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
@@ -7,11 +15,13 @@ _Context* do_syscall(_Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
- // Log("%d  %d  %d  %d\n",a[0],a[1],a[2],a[3]);
+  Log("%d  %d  %d  %d\n",a[0],a[1],a[2],a[3]);
   switch (a[0]) {
 	case SYS_exit: _halt(a[1]);c->GPR1=0;break;
 	case SYS_yield: _yield();c->GPR1=0;break;
-    case SYS_write:
+    case SYS_open: c->GPR1=fs_open((char *)a[1],0,0);break;
+    case SYS_read: fs_read(a[1],(char*)a[2],a[3]);
+ 	case SYS_write:
     //*****SYS_write****	
 	//intptr_t ret=0;
 	if((int)a[1]==1||(int)a[1]==2){
@@ -25,6 +35,8 @@ _Context* do_syscall(_Context *c) {
 	//c->GPR1=ret;
     break;	
 	//******************
+	case SYS_close: c->GPR1=fs_close(a[1]);break;
+    case SYS_lseek: c->GPR1=fs_lseek(a[1],a[2],a[3]);
 	case SYS_brk:/*Log("%d",a[1]);*/ c->GPR1=0; break;
   	default: panic("Unhandled syscall ID = %d", a[0]);
   }
