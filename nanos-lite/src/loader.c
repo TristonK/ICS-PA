d6402ee733f879dc870114f33fbddbbe8c619bcc
tracer-ics2018 <tracer@njuics.org>
2018-12-29 22:17:18 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:17:18 up 14:17,  8 users,  load average: 0.65, 0.24, 0.21 a771ac6afdb6d9478741278247efac3a33524

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,6 @@
 #include "proc.h"
 
-#define DEFAULT_ENTRY 0x4000000
+#define DEFAULT_ENTRY 0x8048000
 
 #include <sys/types.h>
 int fs_open(const char *pathname, int flags ,int mode);
@@ -8,7 +8,7 @@ ssize_t fs_read(int fd, void *buf, size_t len);
 ssize_t fs_write(int fd, void *buf, size_t len);
 int fs_close(int fd);
 size_t fs_filesz(int fd);
-
+int _protect(_Protect *p);
 static uintptr_t loader(PCB *pcb, const char *filename) {
 //  TODO();
 // size_t len = get_ramdisk_size();
@@ -27,7 +27,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
 void naive_uload(PCB *pcb, const char *filename) {
 //	Log("here");
-  uintptr_t entry = loader(pcb, filename);
+//  _protect(pcb);
+	uintptr_t entry = loader(pcb, filename);
   ((void(*)())entry) ();
 }
 
