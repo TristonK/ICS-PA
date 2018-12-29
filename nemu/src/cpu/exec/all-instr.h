d27f0c11a3e07fac7e26fa7f4a736d3f0aab8924
tracer-ics2018 <tracer@njuics.org>
2018-12-29 19:58:35 +0800

    >  compile 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:58:35 up 11:58,  8 users,  load average: 0.50, 0.48, 0.45 def5e0dbf596d644d00f2c3bf0290ae0c520963e

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -52,3 +52,5 @@ make_EHelper(iret);
 make_EHelper(lidt);
 make_EHelper(pusha);
 make_EHelper(popa);
+make_EHelper(mov_r2cr);
+make_EHelper(mov_cr2r);
--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -18,13 +18,28 @@ else if(id_dest->width==4)
 }
 
 make_EHelper(mov_r2cr) {
-  TODO();
-
+//  TODO();
+  if(id_dest->reg==0)
+	  cpu.cr0.val=id_src->val;
+  else if(id_dest->reg==3)
+	  cpu.cr3.val=id_src->val;
+  else
+	  assert(0);
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-  TODO();
+  //TODO();
+  if(id_src->reg==0){
+	  t1=cpu.cr0.val;
+  }
+  else if(id_src->reg==3){
+	  t1=cpu.cr3.val;
+  }
+  else 
+	  assert(0);
+  operand_write(id_dest,&t1);
+
 
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
