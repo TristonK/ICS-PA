358acd3e767abbb0e138645457d115b0c047b03a
tracer-ics2018 <tracer@njuics.org>
2019-02-08 11:08:14 +0800

    >  run 171240501 ksl Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:08:14 up 1 day,  6:57,  4 users,  load average: 0.51, 0.37, 0.44 e155b90bf2834407872c8ebd7bdc843a483ed251

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -18,18 +18,19 @@ int mm_brk(uintptr_t new_brk) {
 	if(current->cur_brk == 0){
 		current->cur_brk = current->max_brk = new_brk;
 	}
-	else{
+//	else{
 		if(new_brk > current->max_brk){
 			uintptr_t va_begin = /*PGROUNDUP(current->max_brk);*/(current->max_brk+0xfff)&(~0xfff);
 			uintptr_t va_end = new_brk;
-			for(uintptr_t va = va_begin; va <= va_end; va+=PGSIZE){
+			uintptr_t va=va_begin;
+			for(;va < va_end; va+=PGSIZE){
 				void* pa = new_page(1);
 				_map(&current->as,(void*)va,pa,1);
 			}
-			current->max_brk = new_brk;
+			current->max_brk = va;//new_brk;
 		}
-		current->cur_brk = new_brk;
-	}
+		current->cur_brk = (new_brk&0xfff)?new_brk+PGSIZE:new_brk;
+//	}
 	return 0;
 }
 
